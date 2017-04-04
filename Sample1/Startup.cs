7db4ef53c9f88730a4b332aa7af13748303f5e10
Aram Kocharyan <aram.koch@gmail.com>
2017-04-04 17:32:57 +0400

    Add project files.

---
+++ Sample1/Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using BlobService.Core.Configuration;
+using BlobService.MetaStore.EntityFrameworkCore.Configuration;
+using BlobService.Storage.FileSystem.Configuration;
+
+namespace Sample1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var builder = services.AddBlobService(opts =>
+            {
+                opts.MaxBlobSizeInMB = 100;
+            })
+            .AddEfMetaStores(opts =>
+            {
+                opts.ConnectionString = "Server=myServerAddress;Database=myDataBase;User Id=myUsername;Password = myPassword;";
+            })
+            .AddFileSystemStorageService(opts =>
+            {
+                opts.RootPath = @"C:\blobs";
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole();
+
+            app.UseBlobService();
+        }
+    }
+}
